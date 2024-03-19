49bdf6d61d83eaed8d245ad1259413a07b485b8f
GayathriDiggibyte <gayathri.m@diggibyte.com>
2024-03-19 10:03:50 +0530

    Assignment3

--- src/Assignment3.sql
+++ src/Assignment3.sql
@@ -1,18 +1,21 @@
-#create database for assignment3
-create database ass3;
-use ass3;
-#create table dept_tbl
-create table dept_tbl(id_deptname varchar(50), emp_name varchar(30), salary int);
-#inserting records to dept_tbl
-insert into dept_tbl values('1111-MATH', 'RAHUL', 10000),
-('1111-MATH', 'RAKESH', 20000),
-('2222-SCIENCE', 'AKASH', 10000),
-('222-SCIENCE', 'ANDREW', 10000),
-('22-CHEM', 'ANKIT', 25000),
-('3333-CHEM', 'SONIKA', 12000),
-('4444-BIO', 'HITESH', 2300),
-('44-BIO', 'AKSHAY', 10000);
-select * from dept_tbl;
-#Find the total salary of each department
-select substring_index(id_deptname,'-',-1) as dept_name,sum(salary) as total_salary from dept_tbl group by substring_index(id_deptname,'-',-1);
+#create database for assignment2
+create database ass2;
+use ass2;
+#create table product_details
+create table product_details(sell_date DATE,product varchar(30));
+#inserting records to product_details
+delete from product_details;
+insert into product_details values('2020-05-30', 'Headphones'),
+('2020-06-01','Pencil'),
+('2020-06-02','Mask'),
+('2020-05-30','Basketball'),
+('2020-06-01','Book'),
+('2020-06-02', 'Mask'),
+('2020-05-30','T-Shirt');
+#displaying all records from product_details
+select * from product_details;
+#Write a query to find for each date the number of different products sold and their names. 
+select sell_date,count(distinct product) as num_of_products,
+group_concat(distinct product) as product_names 
+from product_details group by sell_date;
 
